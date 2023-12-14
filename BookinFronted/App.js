238600ce5531b10a33ef8a0487ae3557b1e9b623
kyle-liminal <kyle@liminal.sh>
2023-12-14 14:28:35 -0800

    sets up to be used with expo go

---
+++ BookinFronted/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  console.log("hello");
+  return (
+    <View style={styles.container}>
+        <Text>Open up App.js to start working on your ting !</Text>
+        <Button title="button"/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
